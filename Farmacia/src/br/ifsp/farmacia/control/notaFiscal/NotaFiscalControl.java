dce1e23fce5adf0e1352e2f61fe288fcc8539e5c
Consoli <matheus.consoli7@gmail.com>
2018-09-26 03:07:15 -0300

    Nota fiscal criada

--- Farmacia/src/br/ifsp/farmacia/control/notaFiscal/NotaFiscalControl.java
+++ Farmacia/src/br/ifsp/farmacia/control/notaFiscal/NotaFiscalControl.java
@@ -1,19 +1,33 @@
 package br.ifsp.farmacia.control.notaFiscal;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import br.ifsp.farmacia.model.entities.NotaFiscal;
 import br.ifsp.farmacia.model.persistence.notaFiscal.INotaFiscalDAO;
+import br.ifsp.farmacia.model.persistence.notaFiscal.NotaFiscalDAO;
 
 public class NotaFiscalControl implements INotaFiscalDAO {
 
+	NotaFiscalDAO nfeDao = new NotaFiscalDAO();
+	
 	@Override
-	public void emitirNotaFiscal(NotaFiscal nfe) {
-		// TODO Auto-generated method stub
-		
+	public boolean emitirNotaFiscal(NotaFiscal nfe) throws SQLException {
+		return nfeDao.emitirNotaFiscal(nfe);
 	}
 
 	@Override
-	public void cancelarNotaFiscal(NotaFiscal nfe) {
-		// TODO Auto-generated method stub
-		
+	public boolean cancelarNotaFiscal(NotaFiscal nfe) throws SQLException {
+		return nfeDao.cancelarNotaFiscal(nfe);
+	}
+
+	@Override
+	public ArrayList<NotaFiscal> selectNotaFiscal() throws SQLException {
+		return nfeDao.selectNotaFiscal();
+	}
+
+	@Override
+	public ArrayList<NotaFiscal> selectNotaFiscal(String filtr) throws SQLException {
+		return nfeDao.selectNotaFiscal(filtr);
 	}
 }
