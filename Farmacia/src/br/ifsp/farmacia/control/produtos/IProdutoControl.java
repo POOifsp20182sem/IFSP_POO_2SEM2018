654a40529da5e4e110d55736859f70839f4eec93
Éder Oliveira <ederoliveirabarreto@gmail.com>
2018-10-02 16:23:40 -0300

    arquivos desnecessarios removidos

--- Farmacia/src/br/ifsp/farmacia/control/produtos/IProdutoControl.java
+++
@@ -1,8 +0,0 @@
-package br.ifsp.farmacia.control.produtos;
-
-public interface IProdutoControl {
-
-	/***
-	 * Definir os métodos que ela deve implementar para organização
-	 */
-}
--- Farmacia/src/br/ifsp/farmacia/control/vendas/IVendaControl.java
+++
@@ -1,5 +0,0 @@
-package br.ifsp.farmacia.control.vendas;
-
-public interface IVendaControl {
-
-}
