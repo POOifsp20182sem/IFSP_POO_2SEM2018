a728e40826825989b788408050a6bb04cc1b66d5
Alice Lima <alice_lima2@live.com>
2018-10-24 02:14:56 -0300

    Classe PedidosPedidos

--- BD/farmacia_db.sql
+++ BD/farmacia_db.sql
@@ -4,7 +4,7 @@ USE `farmacia`;
 --
 -- Host: localhost    Database: farmacia
 -- ------------------------------------------------------
--- Server version	5.7.19-log
+-- Server version	5.6.17
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -28,7 +28,7 @@ CREATE TABLE `classe_terapeutica` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=81 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,7 +37,7 @@ CREATE TABLE `classe_terapeutica` (
 
 LOCK TABLES `classe_terapeutica` WRITE;
 /*!40000 ALTER TABLE `classe_terapeutica` DISABLE KEYS */;
-INSERT INTO `classe_terapeutica` VALUES (1,'teste'),(2,'Adstringente'),(3,'Amebicidas'),(4,'Bateriostático'),(5,'Broncodilatadores'),(6,'Calcitoninas'),(7,'Cicatrizante'),(8,'Descongestionante'),(9,'Diuréticos'),(10,'Emoliente'),(11,'Expectorantes'),(12,'Fibrinogênio'),(13,'Fibratos'),(14,'Gangliosideo'),(15,'Glicocorticóides'),(16,'Hormônio'),(17,'Hepatoprotetor'),(18,'Inibidores Fibrinólise'),(19,'Imunoglobulina Tetânica'),(20,'Laxante'),(21,'Lubrificante Oftálmico'),(22,'Monobactâmicos'),(23,'Mucolítico'),(24,'Nootrópicos'),(25,'Nitritos e Nitratos'),(26,'Orexígenos'),(27,'Polimixinas'),(28,'Pediculicida Sarnicida'),(29,'Quinolonas Urinários'),(30,'Quimioterápico Tópico'),(31,'Radiofármacos'),(32,'Regulador Intestinal'),(33,'Sedativo Hipnótico'),(34,'Soluções Aminoácidas Padrão'),(35,'Tecidos Hemostáticos'),(36,'Tranquilizantes'),(37,'Vasodilatadores'),(38,'Vasopressor'),(78,'classe 1'),(79,'classe 2'),(80,'classe 3');
+INSERT INTO `classe_terapeutica` VALUES (1,'teste');
 /*!40000 ALTER TABLE `classe_terapeutica` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -61,7 +61,7 @@ CREATE TABLE `cliente` (
   `data_nascimento` date NOT NULL,
   `ativo` tinyint(1) DEFAULT '1',
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=29 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -70,7 +70,7 @@ CREATE TABLE `cliente` (
 
 LOCK TABLES `cliente` WRITE;
 /*!40000 ALTER TABLE `cliente` DISABLE KEYS */;
-INSERT INTO `cliente` VALUES (7,'zé mane','eder@gmail.com','Rua x','3534652870','99999999999','fisica','12409536619',NULL,'2000-12-12',0),(8,'zé mane','ze@gmail.com','Rua 7','123213213','5454848444','juridica',NULL,'12409536619123','2000-10-15',0),(10,'Batman','eder@gmail.com','Rua x','3534652870','99999999999','fisica','12409536619',NULL,'2000-12-12',0),(11,'Éder','eder@gmail.com','Rua x','3534652870','99999999999','fisica','12409536619',NULL,'2000-12-12',0),(14,'Éder','eder@gmail','Monte Sião, Irineu Bernardi, 97, ','1988845','154544','fisica','12409534612','','1995-00-04',0),(15,'Éder','eder@gmail','Monte Sião, Irineu Bernardi, 97, ','1988845','154544','juridica','','12409534612','1995-00-04',0),(16,'Éder','eder@gmail','Monte Sião, Irineu Bernardi, 97, ','1988845','154544','juridica','','12409534612','1995-00-04',0),(17,'aaa','asdasd','rua x','145454','45454','fisica','cpf',NULL,'1992-12-12',0),(20,'eder','adsad','asdasd','1213','12423','juridica',NULL,'1asdasd','1992-12-12',0),(21,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',0),(22,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',0),(23,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',0),(24,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',0),(25,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',0),(26,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',0),(27,'eder','der','c, a, b, d','2222222222','33333333333','fisica',NULL,NULL,'1111-11-11',0),(28,'joao','joao@lala.com','alameda, flor, caixa,34','1187654321','1109876543','fisica','12345678901',NULL,'2000-09-16',1);
+INSERT INTO `cliente` VALUES (7,'zé mane','eder@gmail.com','Rua x','3534652870','99999999999','fisica','12409536619',NULL,'2000-12-12',0),(8,'zé mane','ze@gmail.com','Rua 7','123213213','5454848444','juridica',NULL,'12409536619123','2000-10-15',1),(10,'Batman','eder@gmail.com','Rua x','3534652870','99999999999','fisica','12409536619',NULL,'2000-12-12',1),(11,'Éder','eder@gmail.com','Rua x','3534652870','99999999999','fisica','12409536619',NULL,'2000-12-12',1),(14,'Éder','eder@gmail','Monte Sião, Irineu Bernardi, 97, ','1988845','154544','fisica','12409534612','','1995-00-04',1),(15,'Éder','eder@gmail','Monte Sião, Irineu Bernardi, 97, ','1988845','154544','juridica','','12409534612','1995-00-04',1),(16,'Éder','eder@gmail','Monte Sião, Irineu Bernardi, 97, ','1988845','154544','juridica','','12409534612','1995-00-04',1),(17,'aaa','asdasd','rua x','145454','45454','fisica','cpf',NULL,'1992-12-12',1),(20,'eder','adsad','asdasd','1213','12423','juridica',NULL,'1asdasd','1992-12-12',1),(21,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',1),(22,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',1),(23,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',1),(24,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',1),(25,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',1),(26,'eder','eder','c, a, b, d','(11) 11111','(11) 111111','fisica',NULL,NULL,'1111-11-11',1),(27,'eder','der','c, a, b, d','2222222222','33333333333','fisica',NULL,NULL,'1111-11-11',1),(28,'Alice','',' uhifhii, jfdvjk,  ijhfuh,  nsdjv','9873298743','93874737478','fisica',NULL,NULL,'1999-01-02',1),(29,'Alice','alice@gmail.com','Munhoz, Rua G, 65, jbfvhbfe','7231461541','89361460626','fisica',NULL,NULL,'1999-01-02',1),(30,'bjbchvdbas','hbfjhjvfh','vewwf, bfbvher, 44, fhbfhrej','6123864632','','fisica',NULL,NULL,'1999-03-12',1),(31,'bjbchvdbas','hbfjhjvfh','vewwf, bfbvher, 44, fhbfhrej','6123864632','','fisica',NULL,NULL,'1999-03-12',1),(32,'bjbchvdbas','hbfjhjvfh','vewwf, bfbvher, 44, fhbfhrej','6123864632','','fisica',NULL,NULL,'1999-03-12',1);
 /*!40000 ALTER TABLE `cliente` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -150,7 +150,7 @@ CREATE TABLE `login` (
   `nome` varchar(40) NOT NULL,
   `senha` varchar(40) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -159,7 +159,7 @@ CREATE TABLE `login` (
 
 LOCK TABLES `login` WRITE;
 /*!40000 ALTER TABLE `login` DISABLE KEYS */;
-INSERT INTO `login` VALUES (1,'eder','123'),(2,'consoli','321'),(3,'admin','admin');
+INSERT INTO `login` VALUES (1,'eder','123'),(2,'consoli','321');
 /*!40000 ALTER TABLE `login` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -247,7 +247,7 @@ CREATE TABLE `pedido` (
   KEY `funcionario_id` (`funcionario_id`),
   CONSTRAINT `pedido_ibfk_1` FOREIGN KEY (`cliente_id`) REFERENCES `cliente` (`id`),
   CONSTRAINT `pedido_ibfk_2` FOREIGN KEY (`funcionario_id`) REFERENCES `funcionario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -256,7 +256,7 @@ CREATE TABLE `pedido` (
 
 LOCK TABLES `pedido` WRITE;
 /*!40000 ALTER TABLE `pedido` DISABLE KEYS */;
-INSERT INTO `pedido` VALUES (2,'2005-10-29',7,2,100,0.1);
+INSERT INTO `pedido` VALUES (2,'2005-10-29',7,2,100,0.1),(7,'2018-10-23',8,5,40,1);
 /*!40000 ALTER TABLE `pedido` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -271,7 +271,7 @@ CREATE TABLE `principio_ativo` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `nome` varchar(100) DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=45 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -280,7 +280,7 @@ CREATE TABLE `principio_ativo` (
 
 LOCK TABLES `principio_ativo` WRITE;
 /*!40000 ALTER TABLE `principio_ativo` DISABLE KEYS */;
-INSERT INTO `principio_ativo` VALUES (1,'teste'),(2,'Acebrofilina'),(3,'Aceclofenaco'),(4,'Baclofeno'),(5,'Benzocaína'),(6,'Calcitriol'),(7,'Carbocisteína'),(8,'Deltametrina'),(9,'Desonida'),(10,'Ebastina'),(11,'Estradiol'),(12,'Fentanila'),(13,'Fenitoína'),(14,'Glicina'),(15,'Glicerina'),(16,'Halotano'),(17,'Haloperidol'),(18,'Ibuprofeno'),(19,'Icodextrina'),(20,'Jaborandi'),(21,'Lactase'),(22,'Lansoprazol'),(23,'Macitentana'),(24,'Magaldrato'),(25,'Nicotina'),(26,'Nicergolina'),(27,'Ocitocina'),(28,'Oxitocina'),(29,'Paracetamol'),(30,'Paricalcitol'),(31,'Quitosana'),(32,'Quelato de Cálcio'),(33,'Roxitromicina'),(34,'Secnidazol'),(35,'Simeticona'),(36,'Teofilina'),(37,'Tianeptina'),(38,'Ureia'),(39,'Urtiga Dioica'),(40,'Xilometazolina'),(41,'Zinco'),(42,'principio 1'),(43,'principio 2'),(44,'principio 3');
+INSERT INTO `principio_ativo` VALUES (1,'teste');
 /*!40000 ALTER TABLE `principio_ativo` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -332,18 +332,17 @@ DROP TABLE IF EXISTS `sessao`;
 CREATE TABLE `sessao` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `func_abertura_id` int(11) NOT NULL,
-  `func_fechamento_id` int(11) DEFAULT NULL,
+  `func_fechamento_id` int(11) NOT NULL,
   `saldo_inicial` double NOT NULL,
-  `saldo_final` double DEFAULT NULL,
+  `saldo_final` double NOT NULL,
   `data_abertura` datetime DEFAULT NULL,
   `data_fechamento` datetime DEFAULT NULL,
-  `status` enum('ABERTO','FECHADO') NOT NULL,
   PRIMARY KEY (`id`),
   KEY `func_abertura_id` (`func_abertura_id`),
   KEY `func_fechamento_id` (`func_fechamento_id`),
   CONSTRAINT `sessao_ibfk_1` FOREIGN KEY (`func_abertura_id`) REFERENCES `funcionario` (`id`),
   CONSTRAINT `sessao_ibfk_2` FOREIGN KEY (`func_fechamento_id`) REFERENCES `funcionario` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -352,13 +351,8 @@ CREATE TABLE `sessao` (
 
 LOCK TABLES `sessao` WRITE;
 /*!40000 ALTER TABLE `sessao` DISABLE KEYS */;
-INSERT INTO `sessao` VALUES (1,2,2,1000,2000,'0000-00-00 00:00:00','0000-00-00 00:00:00','FECHADO'),(2,2,NULL,1000,NULL,'2018-09-25 00:00:00','0000-00-00 00:00:00','FECHADO'),(4,2,NULL,1000,NULL,'2018-09-25 21:21:21',NULL,'FECHADO'),(5,2,NULL,1000,NULL,'2018-09-25 21:21:21',NULL,'FECHADO'),(6,2,NULL,1000,NULL,'2018-09-25 21:21:21',NULL,'ABERTO');
 /*!40000 ALTER TABLE `sessao` ENABLE KEYS */;
 UNLOCK TABLES;
-
---
--- Dumping events for database 'farmacia'
---
 
 --
 -- Dumping routines for database 'farmacia'
@@ -698,37 +692,6 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `buscar_movimento` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `buscar_movimento`(
-IN filter varchar(50))
-BEGIN
-	IF filter LIKE '' THEN
-		SELECT id, sessao_id, pedido_id, descricao, data_movimento, valor_entrada, valor_saida, troco, saldo, forma_pagamento, nota_fiscal_id
-        FROM movimento;
-	ELSE
-    #Por enquanto a pesquisa vai ficar simples, posteriormente pesquisar por período
-		SELECT id, sessao_id, pedido_id, descricao, data_movimento, valor_entrada, valor_saida, troco, saldo, forma_pagamento, nota_fiscal_id
-			FROM movimento
-				WHERE
-					descricao LIKE CONCAT('%', filter,'%') OR
-					forma_pagamento LIKE CONCAT('%', filter,'%') OR
-					nota_fiscal_id LIKE CONCAT('%', filter,'%');
-	END IF;
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `buscar_pedidos` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -737,7 +700,7 @@ DELIMITER ;
 /*!50003 SET character_set_results = utf8 */ ;
 /*!50003 SET collation_connection  = utf8_general_ci */ ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
 CREATE DEFINER=`root`@`localhost` PROCEDURE `buscar_pedidos`(IN filter varchar(50))
 BEGIN
@@ -751,7 +714,7 @@ BEGIN
 				INNER JOIN cliente c on (p.cliente_id = c.id)
 				INNER JOIN funcionario f on (p.funcionario_id = f.id);
 	ELSE
-		SELECT p.id, p.data_compra, p.cliente_id,c.nome, p.funcionario_id,f.nome, p.total, p.desconto
+		SELECT p.id, p.data_compra, p.cliente_id,c.nome, p.funcionario_id,f.nome as 'nome_fun', p.total, p.desconto
 			FROM pedido as p 
 				INNER JOIN cliente c on (p.cliente_id = c.id)
 				INNER JOIN funcionario f on (p.funcionario_id = f.id)
@@ -1086,81 +1049,6 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `inserir_movimento` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `inserir_movimento`(
-IN p_sessao_id int(11),
-IN p_pedido_id int(11),
-IN p_descricao varchar(50),
-IN p_data_movimento datetime,
-IN p_valor_entrada double,
-IN p_valor_saida double,
-IN p_troco double,
-IN p_saldo double,
-IN p_forma_pagamento enum('cartao','dinheiro'),
-IN p_nota_fiscal_id int(11)
-)
-BEGIN
-
-	INSERT INTO movimento (sessao_id, pedido_id, descricao, data_movimento, valor_entrada, valor_saida, troco, saldo, forma_pagamento, nota_fiscal_id) 
-    VALUES 
-    (
-    p_sessao_id,
-    p_pedido_id,
-    p_descricao,
-    p_data_movimento,
-    p_valor_entrada,
-    p_valor_saida,
-    p_troco, 
-    p_saldo,
-    p_forma_pagamento,
-    p_nota_fiscal_id
-    );
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `inserir_nota_fiscal` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `inserir_nota_fiscal`(
-IN p_numero_nf int(11),
-IN p_status_nf enum('AUTORIZADA','CANCELADA','PROCESSANDO'),
-IN p_chave_nf varchar(45),
-IN p_protocolo_nf	varchar(40)
-)
-BEGIN
-	INSERT INTO nota_fiscal (numero_nf, status_nf, chave_nf, protocolo_nf) 
-		VALUES
-        (
-        p_numero_nf,
-        p_status_nf,
-        p_chave_nf,
-        p_protocolo_nf
-        );
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!50003 DROP PROCEDURE IF EXISTS `inserir_pedido` */;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
 /*!50003 SET @saved_cs_results     = @@character_set_results */ ;
@@ -1238,53 +1126,6 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
-/*!50003 DROP PROCEDURE IF EXISTS `salvar_sessao` */;
-/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
-/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
-/*!50003 SET @saved_col_connection = @@collation_connection */ ;
-/*!50003 SET character_set_client  = utf8 */ ;
-/*!50003 SET character_set_results = utf8 */ ;
-/*!50003 SET collation_connection  = utf8_general_ci */ ;
-/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
-/*!50003 SET sql_mode              = 'NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
-DELIMITER ;;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `salvar_sessao`(
-IN p_func_abertura_id int(11),
-IN p_func_fechamento_id int(11),
-IN p_saldo_inicial double,
-IN p_saldo_final double,
-IN p_data_abertura datetime,
-IN p_data_fechamento datetime
-)
-BEGIN
-	set @last_id = (SELECT max(id) FROM sessao);
-
-	IF((SELECT `status` FROM sessao WHERE id = @last_id) LIKE 'FECHADO') THEN
-    
-		INSERT INTO sessao (func_abertura_id, saldo_inicial, data_abertura, status) 
-			VALUES 
-			(
-			p_func_abertura_id,
-			p_saldo_inicial,
-			p_data_abertura,
-			'ABERTO' 
-			);
-	ELSE
-		UPDATE sessao
-        SET
-			func_fechamento_id = p_func_fechamento_id,
-			saldo_final = p_saldo_final,
-			data_fechamento = p_data_fechamento,
-			status = 'FECHADO'
-            WHERE id = @last_id;
-    END IF;
-
-END ;;
-DELIMITER ;
-/*!50003 SET sql_mode              = @saved_sql_mode */ ;
-/*!50003 SET character_set_client  = @saved_cs_client */ ;
-/*!50003 SET character_set_results = @saved_cs_results */ ;
-/*!50003 SET collation_connection  = @saved_col_connection */ ;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -1295,4 +1136,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-09-26 20:48:46
+-- Dump completed on 2018-10-24  2:09:41
